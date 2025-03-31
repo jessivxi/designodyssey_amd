d7e75ee5abacaf6c0d4d12e850ffda3dc4acf130
jessi <155872918+jessivxi@users.noreply.github.com>
2025-03-31 17:23:02 -0300

    Add files via upload

---
+++ DesignOyssey - adm/painel/index.php
@@ -0,0 +1,57 @@
+<?php
+    include "navbar.php";
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style-painel/index.css">
+
+    <title>Dashboard</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="painel_adm">
+        <p class="painel">Painel</p>
+    </div>
+    <div class="boxes_adm_main">
+        <div class="box_gerencer_color_red">
+            <p class="number_gerencer">44</p>
+            <p class="title_gerencer">Gerenciamento <br> de usuario
+            <i class="bi bi-person-fill-check"></i></p>
+        </div>
+        <div class="box_service_color_blue">
+            <p class="number_service">44</p>
+            <p class="title_service">Controle de serviços
+            <i class="bi bi-person-bounding-box"></i>
+            </p>
+        </div>
+        <div class="box_control_color_yellow">
+            <p class="number_control">44</p>
+            <p class="title_control">Controle de vendas
+            <i class="bi bi-bar-chart-fill"></i>
+            </p>
+        </div>
+        <div class="box_suport_color_green">
+            <p class="title_suport">Suporte Tecnico</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16">
+            <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5"/>
+            </svg>
+        </div>
+    </div>
+    <div class="calender_painel_grey_colune">
+        <div class="calender_box_grey">
+            <p class="tittle_calender">Calendario</p>
+            <div class="icon_sgv_calender">
+            <i class="bi bi-calendar-check"></i>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ DesignOyssey - adm/painel/navbar.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar</title>
+    <style>
+        body {
+            margin: 0;
+            flex-direction: row;
+            font-family: Arial, sans-serif;
+        }
+        .sidebar {
+            height: 100vh;
+            width: 250px;
+            background-color:rgb(238, 238, 238);
+            color: black;
+            position: fixed;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding-top: 20px;
+        }
+        .profile-pic {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            background-color:rgb(217, 217, 217);
+            margin-bottom: 10px;
+        }
+        .menu {
+            width: 100%;
+            list-style: none;
+            padding: 0;
+        }
+        .menu li {
+            width: 100%;
+            text-align: center;
+            padding: 15px 0;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            border-bottom: 1px solid #ccc; /* Adiciona o divisor */
+
+        }
+        .menu li a {
+            text-decoration: none;
+            font-size: 13px;
+            color: black;
+            display: block;
+            
+        }
+        .menu a1 {
+            text-decoration: none;
+            font-size: 15px;
+            text-align: center;
+            color: rgba(0, 0, 0, 0.51);
+            display: block;
+        }
+        .menu li a:hover {
+            color: #0096D1;
+        }
+        .divisor_painel_adm{
+            display:block;
+            height:80px;
+            width: auto;
+            border-bottom: 1px solid #ccc; /* Adiciona o divisor */
+
+        }
+    </style>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="profile-pic"></div>
+        <ul class="menu">
+        <a1 href="#">Administrador Odyssey</a1>
+        </ul>
+        <ul class="menu">
+            <li><a href="#">Painel</a></li>
+            <li><a href="#">Gerenciamento de Usuário</a></li>
+            <li><a href="#">Controle de Serviços</a></li>
+            <li><a href="#">Controle de Vendas</a></li>
+            <li><a href="#">Suporte Técnico</a></li>
+        </ul>
+    </div>
+    <div class="divisor_painel_adm"></div>
+</body>
+</html>
